f798465080cb407854802b33ffb23ac9637d9c01
Dennis <denkipyator@gmail.com>
2023-12-20 07:30:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+   return customerName.toUpperCase();
+}
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob';
+   return bestCustomer;
+}
+const leastFavoriteCustomer = 'sheldon';
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = 'george'
+   return leastFavoriteCustomer;
+}
+
+
+ // Write your solution in this file!
